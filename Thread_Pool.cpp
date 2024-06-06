dab7a82c1d9c89be4e2d678e6df2338db1b62b6d
amasa <amasaabubakar@gmail.com>
2024-06-06 12:16:37 +0100

    init

--- Thread_Pool.cpp
+++ Thread_Pool.cpp
@@ -1,33 +1,33 @@
-//#include "Thread_Pool.h"
-//#include <iostream>
-//#include <functional>
-//
-//
-//
-//
-//void Thread_Pool::worker() {
-//	while (true) {
-//		Func task;
-//		wrk_queue.pop(task);
-//		task();
-//	}
-//}
-//
-//Thread_Pool::Thread_Pool() {
-//	//get number of cores
-//	int num_cores = std::thread::hardware_concurrency();
-//	for (int i = 0; i < num_cores; i++) {
-//		threads.push_back(std::thread(&Thread_Pool::worker, this));
-//	}
-//}
-//
-//void Thread_Pool::submit(Func F) {
-//	wrk_queue.push(F);
-//}
-//
-////ensure all threads are joined
-//Thread_Pool::~Thread_Pool() {
-//	for (auto& t : threads) {
-//		t.join();
-//	}
-//}
+#include "Thread_Pool.h"
+#include <iostream>
+#include <functional>
+
+
+
+
+void Thread_Pool::worker() {
+	while (true) {
+		Func task;
+		wrk_queue.pop(task);
+		task();
+	}
+}
+
+Thread_Pool::Thread_Pool() {
+	//get number of cores
+	int num_cores = std::thread::hardware_concurrency();
+	for (int i = 0; i < num_cores; i++) {
+		threads.push_back(std::thread(&Thread_Pool::worker, this));
+	}
+}
+
+void Thread_Pool::submit(Func F) {
+	wrk_queue.push(F);
+}
+
+//ensure all threads are joined
+Thread_Pool::~Thread_Pool() {
+	for (auto& t : threads) {
+		t.join();
+	}
+}
--- Thread_Pool.h
+++ Thread_Pool.h
@@ -1,16 +1,17 @@
-//#pragma once
-//#include <vector>
-//#include <thread>
-//#include "Queue.h"
-//using Func = std::function<void()>;
-//class Thread_Pool
-//{
-//	std::vector<std::thread> threads;
-//	Queue<Func> wrk_queue;
-//	void worker();
-//public:
-//	Thread_Pool();
-//	~Thread_Pool();
-//	void submit(Func f);
-//};
-//
+#pragma once
+#include <vector>
+#include <thread>
+#include <functional>
+#include "Queue.h"
+using Func = std::function<void()>;
+class Thread_Pool
+{
+	std::vector<std::thread> threads;
+	Queue<Func> wrk_queue;
+	void worker();
+public:
+	Thread_Pool();
+	~Thread_Pool();
+	void submit(Func f);
+};
+
